3547bf0b15233fcff328a344d3f14e435849c1ba
hosam <hoadel2003@gmail.com>
2025-03-07 03:20:44 +0200

    drawer and home header done

---
+++ lib/features/home/presentation/views/drawer/drawer_view.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+import 'package:my_visitor/features/home/presentation/views/drawer/custom_list_tile.dart';
+
+class DrawerView extends StatefulWidget {
+  const DrawerView({super.key});
+
+  @override
+  State<DrawerView> createState() => _DrawerViewState();
+}
+
+class _DrawerViewState extends State<DrawerView> {
+  bool isDark = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.white,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          SizedBox(
+            height: 100,
+          ),
+          // CustomListTile(
+          //   icon: Icons.dark_mode,
+          //   title: 'Dark Mode',
+          //   trailing: Switch(
+          //     value: isDark,
+          //     onChanged: (value) {
+          //       setState(() {
+          //         isDark = value;
+          //       });
+          //     },
+          //     inactiveTrackColor: Colors.grey[300],
+          //     inactiveThumbColor: Colors.grey,
+          //   ),
+          //   onTap: () {
+          //     // إضافة ما تود فعله عند الضغط على Dark Mode
+          //   },
+          // ),
+          CustomListTile(
+            icon: Icons.info_outline,
+            title: 'Account Information',
+            onTap: () {},
+          ),
+        
+          CustomListTile(
+            icon: Icons.notifications,
+            title: 'Notification',
+            onTap: () {},
+          ),
+          CustomListTile(
+            icon: Icons.local_play,
+            title: 'My Plans',
+            onTap: () {
+              //GoRouter.of(context).push(OrderView.id);
+            },
+          ),
+
+          CustomListTile(
+            icon: Icons.favorite_outline,
+            title: 'Favourits',
+            onTap: () {},
+          ),
+          CustomListTile(
+            icon: Icons.settings_outlined,
+            title: 'Settings',
+            onTap: () {},
+          ),
+          CustomListTile(
+            icon: Icons.logout,
+            title: 'Logout',
+            color: const Color.fromARGB(255, 180, 32, 21),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:my_visitor/core/utils/assets.dart';
+import 'package:my_visitor/features/home/presentation/views/drawer/drawer_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
-static String id='HomeView';
+  static String id = 'HomeView';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-      Column(
-        children: [
-          
+      appBar: AppBar(
+        // leading: IconButton(
+        //   onPressed: () {},
+        //   icon: Icon(
+        //     Icons.draw_rounded,
+        //   ),
+        // ),
+        title: Image.asset(Assets.imagesHomeLogo),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.person,size: 26,),
+            onPressed: () {
+             
+            },
+          ),
         ],
+      ),
+      drawer: DrawerView(),
+      body: Column(
+        children: [],
       ),
     );
   }
