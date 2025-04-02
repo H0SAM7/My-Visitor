7421374a0075fd4d6faea21ef7470226371a2ce1
hosam <hoadel2003@gmail.com>
2025-04-02 20:19:55 +0200

    create hotel entity

--- lib/features/hotels/domain/entities/hotel_entity.dart
+++ lib/features/hotels/domain/entities/hotel_entity.dart
@@ -1,6 +1,14 @@
 import 'package:my_visitor/features/hotels/data/models/hotel_model/property.dart';
+import 'package:hive/hive.dart';
+
+part 'hotel_entity.g.dart';
+
+
+@HiveType(typeId: 0)
 
 class HotelEntity {
+    @HiveField(0)
+
   List<Property>? properties;
 
 
