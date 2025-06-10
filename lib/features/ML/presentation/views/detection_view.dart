3343d5b552755865f6921e60b85be14eae77fecb
hosam <hoadel2003@gmail.com>
2025-06-10 21:37:10 +0300

    finsh

--- lib/features/ML/presentation/views/detection_view.dart
+++ lib/features/ML/presentation/views/detection_view.dart
@@ -3,11 +3,12 @@ import 'dart:typed_data';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_visitor/core/localization/generated/l10n.dart';
 import 'package:my_visitor/core/styles/text_styles.dart';
 import 'package:my_visitor/core/widgets/loading_widgets.dart';
 import 'package:my_visitor/features/ML/data/historical_data.dart';
+import 'package:my_visitor/features/ML/data/models/transelation_model.dart';
 import 'package:my_visitor/features/ML/presentation/manager/cubit/image_detection_cubit.dart';
-import 'package:my_visitor/generated/l10n.dart';
 
 class DetectionView extends StatelessWidget {
   final Uint8List? imageBytes;
